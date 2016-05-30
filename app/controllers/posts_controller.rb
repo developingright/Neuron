daa575a41ac13943a570247ec1fbdf0baf3faf44
developing_right <mshaheer2003@gmail.com>
2016-05-30 15:54:59 +0530

    Added Comments model and controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,7 @@ class PostsController < ApplicationController
     @posts = Post.all.order("created_at DESC")
   end
   def show
+    @comment= Comment.where(post_id: @post)
   end
   def new
     @post =  current_user.posts.build
